002c6fe0ee62e6ec24aaa964cea5b776665a6646
Edoardo Conti <edoandcode@gmail.com>
2026-02-27 16:54:42 +0100

    debug

--- middleware/auth.js
+++ middleware/auth.js
@@ -17,8 +17,6 @@ function auth(req, res, next) {
         return next();
     }
 
-    console.log("JWT_SECRET_KEY:", process.env.JWT_SECRET_KEY);
-
     const token = req.header('x-auth-token');
     if (!token)
         return res.status(401).json({ error: 'Access denied. No token provided.' });
